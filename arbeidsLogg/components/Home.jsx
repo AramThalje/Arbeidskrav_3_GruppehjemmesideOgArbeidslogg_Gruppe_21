082f833574e54f0d27a9933d3d3c7e6aafa87681
AramThalje <Taljeharam@gmail.com>
2025-04-17 15:45:03 +0200

    SetUpReactRouteing

--- arbeidsLogg/components/Home.jsx
+++ arbeidsLogg/components/Home.jsx
@@ -3,17 +3,17 @@ export default function Home(){
         <>
         <h2>Gruppemedlemmer</h2>
         <article>
-            <img src="#" alt="" />
+            <img src={"../vite.svg"} className="articleImg" />  
             <p>navn til Bruker1</p>
             <p>Email til Bruker1</p>
         </article>
         <article>
-            <img src="#" alt="" />
+            <img src={"../vite.svg"} className="articleImg" />  
             <p>navn til Bruker2</p>
-            <p>Email til Bruker2</p>
+            <p><a href=""></a>Email til Bruker2</p>
         </article>
         <article>
-            <img src="#" alt="" />
+            <img src={"../vite.svg"} className="articleImg" />  
             <p>navn til Bruker3</p>
             <p>Email til Bruker4</p>
         </article>
