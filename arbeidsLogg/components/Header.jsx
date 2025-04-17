78f2ec8f56ec936d1265a7f7343d22322460c9c2
Kudu4Ever <111729786+Kudu4Ever@users.noreply.github.com>
2025-04-17 16:22:25 +0200

    update

--- arbeidsLogg/components/Header.jsx
+++ arbeidsLogg/components/Header.jsx
@@ -4,10 +4,10 @@ export default function Header(){
             <p>Group21</p>
             <nav>
                 <ul>
-                    <li>Home</li>
-                    <li>Majed</li>
-                    <li>Aram</li>
-                    <li>Mahmmoud</li>   
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Majed</a></li>
+                    <li><a href="#">Aram</a></li>
+                    <li><a href="#">Mahmmoud</a></li>   
                 </ul>
             </nav>
         </header>
