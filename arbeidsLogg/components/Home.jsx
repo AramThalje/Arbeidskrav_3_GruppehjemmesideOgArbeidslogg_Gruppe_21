16b14a2df1399cf1bf289ffa1d2a68b93b02686b
AramThalje <Taljeharam@gmail.com>
2025-04-17 14:54:23 +0200

    Oppdatering

---
+++ arbeidsLogg/components/Home.jsx
@@ -0,0 +1,22 @@
+export default function Home(){
+    return (
+        <>
+        <h2>Gruppemedlemmer</h2>
+        <article>
+            <img src="#" alt="" />
+            <p>navn til Bruker1</p>
+            <p>Email til Bruker1</p>
+        </article>
+        <article>
+            <img src="#" alt="" />
+            <p>navn til Bruker2</p>
+            <p>Email til Bruker2</p>
+        </article>
+        <article>
+            <img src="#" alt="" />
+            <p>navn til Bruker3</p>
+            <p>Email til Bruker4</p>
+        </article>
+        </>
+    )
+}
